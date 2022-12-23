a4784532479618340f78f31a0678fa01889b48a5
Mark Sayers <marksayers46@icloud.com>
2022-12-22 19:20:16 -0600

    server setup

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-
+// route handler/controller
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
 
+// 404 for catching all other routes use *
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
+        res.sendFile(path.join(__dirname, '/views', '404.html'))
     }else if (req.accepts('json')) {
         res.json({message: '404 Not Found'})
     }else {
