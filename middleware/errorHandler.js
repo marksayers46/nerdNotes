2808cce3a3d88b9f4459cf7473e5b773298bca1d
Mark Sayers <marksayers46@icloud.com>
2022-12-24 10:03:30 -0600

    adding error handler middleware function

---
+++ middleware/errorHandler.js
@@ -0,0 +1,2 @@
+const { logEvents } = require('./logger')
+
--- routes/root.js
+++ routes/root.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-// regex expression root only or /index with the html optional(fun regex)
+// regex expression root only or /index with the html? optional(fun regex)
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '/views', 'index.html'))
 })
